b98aba57f5c37b74e2e13ca8a32eee824d06e85f
SleepingAran <cheahpehao@gmail.com>
2017-02-17 09:38:23 +0800

    Set triangle vertice, add pointer array and buffer

--- Tutorial 2/main.cpp
+++ Tutorial 2/main.cpp
@@ -96,4 +96,27 @@ void main()
 	}
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+
+	//Set each vertices of the triangle
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	//Setup pointer array and buffer
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	//Bind the Vertex Array object first, then bind and set vertex buffer and attribute pointer
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
